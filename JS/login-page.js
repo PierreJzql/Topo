5ed3f666e361e817c4242b2704dd9870781f5553
PierreJzql <pierrejezequel92@gmail.com>
2023-03-30 16:58:14 +0200

    login test Ok, ajout creation.html

--- JS/login-page.js
+++ JS/login-page.js
@@ -8,9 +8,10 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
+    if (username === "pjzql" && password === "on_dev") {
+        // alert("You have successfully logged in.");
+        window.location.href = "Creation.html"
+        // location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
