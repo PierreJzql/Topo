a89afad7471a0db19aa0b1d3e549e700dcb1dbfa
PierreJzql <pierrejezequel92@gmail.com>
2023-04-17 08:51:43 +0200

    mise à jour json et JS

--- JS/Flessel.js
+++ JS/Flessel.js
@@ -1,3 +1,19 @@
+let colorRoad = {
+    "Orange" : "#F39C12",
+    "Blanche" : "#FFFFFF",
+    "Verte" : "#52BE80",
+    "Violette" : "#A569BD",
+    "Rouge" : "#E74C3C",
+    "Rose" : "#FA8DF3",
+    "Jaune" : "#F4D03F",
+    "Bleue" : "#3924D4",
+    "Noire" : "#000000",
+    "Grise" : "#8A898B"
+}
+console.log("Orange : #F39C12 =",colorRoad.Orange)
+console.log("Grise : #8A898B =",colorRoad.Grise)
+
+
 async function voieFlesselApi(){
 const reponseApi = await fetch("../Data/Flessel.json");
 const voiesFlessel = await reponseApi.json();
@@ -7,7 +23,6 @@ return voiesFlessel
 
 function generateVoies (affichage) {
     for (let i = 0; i< affichage.length; i++){
-        
         const voie = affichage[i];
         
         const sectionVoie = document.querySelector(".voieFlessel");
@@ -34,26 +49,29 @@ function generateVoies (affichage) {
         for (let j=0 ; j<voie.voie.length ; j++)
         {
             const voieParam= voie.voie[j];
+           const couleur =  toString(voie.voie[j].color);
+           console.log(colorRoad.couleur)
 
             const colorVoie = document.createElement("p");
             colorVoie.setAttribute("class","tittleVoie Color");
-            colorVoie.innerHTML = `<ul></ul>${voieParam.color} :`;
-            colorVoie.style.color = voieParam.hex
+            colorVoie.innerHTML = voieParam.cotationNumber+voieParam.cotationLetter;
+            // colorVoie.innerHTML = `<ul></ul>${voieParam.color} :`;
+            colorVoie.style.color = voieParam.hex;
 
-            const cotation = document.createElement("li");
-            cotation.innerHTML =`<span>cotation :</span> ${voieParam.cotationNumber+voieParam.cotationLetter}`
+            // const cotation = document.createElement("li");
+            // cotation.innerHTML =`<span>cotation :</span> ${voieParam.cotationNumber+voieParam.cotationLetter}`
 
-            const forbidden = document.createElement("li");
-            if(voieParam.forbidden===""){
-                forbidden.innerHTML = "<span>Interdit :</span> Aucun"
+            // const forbidden = document.createElement("li");
+            // if(voieParam.forbidden===""){
+            //     forbidden.innerHTML = "<span>Interdit :</span> Aucun"
 
-            }else{
-            forbidden.innerHTML = `<span>Interdit :</span> ${voieParam.forbidden}`}
+            // }else{
+            // forbidden.innerHTML = `<span>Interdit :</span> ${voieParam.forbidden}`}
             
             
             voieElement.appendChild(colorVoie);
-            voieElement.appendChild(cotation);
-            voieElement.appendChild(forbidden)
+            // voieElement.appendChild(cotation);
+            // voieElement.appendChild(forbidden)
         }
 
         
