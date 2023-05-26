4efc21e880bb5d1c4ecc58b6f2261d844286fb3b
PierreJzql <pierrejezequel92@gmail.com>
2023-05-26 12:13:45 +0200

    fix color hex

--- JS/Flessel.js
+++ JS/Flessel.js
@@ -1,17 +1,20 @@
-let colorRoad = {
-    "Orange" : "#F39C12",
-    "Blanche" : "#FFFFFF",
-    "Verte" : "#52BE80",
-    "Violette" : "#A569BD",
-    "Rouge" : "#E74C3C",
-    "Rose" : "#FA8DF3",
-    "Jaune" : "#F4D03F",
-    "Bleue" : "#3924D4",
-    "Noire" : "#000000",
-    "Grise" : "#8A898B"
+const colorRoad = {
+    Orange : "#F39C12",
+    Blanche : "#FFFFFF",
+    Verte : "#52BE80",
+    Violette : "#A569BD",
+    Rouge : "#E74C3C",
+    Rose : "#FA8DF3",
+    Jaune : "#F4D03F",
+    Bleue : "#3924D4",
+    Noire : "#000000",
+    Grise : "#8A898B"
 }
 console.log("Orange : #F39C12 =",colorRoad.Orange)
-console.log("Grise : #8A898B =",colorRoad.Grise)
+// console.log("Grise : #8A898B =",colorRoad.Grise)
+
+// for(let key in colorRoad)
+// console.log(key + ': ' + colorRoad[key]);
 
 
 async function voieFlesselApi(){
@@ -49,14 +52,23 @@ function generateVoies (affichage) {
         for (let j=0 ; j<voie.voie.length ; j++)
         {
             const voieParam= voie.voie[j];
-           const couleur =  toString(voie.voie[j].color);
-           console.log(colorRoad.couleur)
+            // const couleur =  toString(voie.voie[j].color);
+            const couleur =  voie.voie[j].color;
+
+            let hex = colorRoad[couleur]
+            
+            // console.log("CL voie.voie[j] =",voie.voie[j].color)
+        //    console.log("CL couleur = ",typeof couleur);
+        //    console.log("CL colorRoad = ",colorRoad[couleur]);
+           
+        //    console.log("CL colorRoad.couleur = ",colorRoad[couleur]);
 
             const colorVoie = document.createElement("p");
             colorVoie.setAttribute("class","tittleVoie Color");
             colorVoie.innerHTML = voieParam.cotationNumber+voieParam.cotationLetter;
             // colorVoie.innerHTML = `<ul></ul>${voieParam.color} :`;
-            colorVoie.style.color = voieParam.hex;
+            // colorVoie.style.color = voieParam.hex;
+            colorVoie.style.color = hex;
 
             // const cotation = document.createElement("li");
             // cotation.innerHTML =`<span>cotation :</span> ${voieParam.cotationNumber+voieParam.cotationLetter}`
